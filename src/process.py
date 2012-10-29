01e23c07eb58e984a0eb14a94afbcf4ad591d800
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-10-28 20:05:22 -0700

    refined command handling

--- src/process.py
+++ src/process.py
@@ -3,13 +3,20 @@ from subprocess import Popen, PIPE
 class TmuxExecError(RuntimeError):
     pass
 
-class Tmux(object):
+class TmuxExecutor(object):
     def __init__(self, socket):
         self.socket = socket
         self.cmd = ['/usr/bin/env', 'tmux', '-S', socket]
     
-    def run(self, command):
-        p = Popen(self.cmd + [command], stdout=PIPE, stderr=PIPE)
+    def __call__(self, command):
+        if not isinstance(command, list):
+            command = [command]
+
+        cmd = self.cmd[:]
+        cmd.append('--')
+        cmd.extend(command)
+
+        p = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out, err = p.communicate()
 
         if p.returncode != 0:
@@ -18,4 +25,5 @@ class Tmux(object):
         return out
 
 if __name__== '__main__':
-    print Tmux('/tmp/tmux-1000/default').run('listxsessions')
+    tmux = TmuxExecutor('/tmp/tmux-1000/default')
+    print tmux('list-sessions')
