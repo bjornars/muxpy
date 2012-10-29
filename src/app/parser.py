11f1e3291d1fdc6c1cc5facb2cfd5acaa4847fb2
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-10-28 22:37:41 -0700

    Renamed tmux.py to backend.py

--- src/app/parser.py
+++ src/app/parser.py
@@ -1,12 +1,12 @@
-from .tmux import Tmux
+from .backend import Backend
 
 class TmuxParser(object):
-    def __init__(self, socket):
-        self.tmux = Tmux(socket)
+    def __init__(self, socket=None, backend=Backend):
+        self.backend = backend(socket)
 
     def list_sessions(self):
-        sessions = self.tmux.list_sessions()
-        print sessions
+        sessions = self.backend.list_sessions()
+        return sessions
 
 if __name__== '__main__':
     tmux = TmuxParser('/tmp/tmux-1000/default')
--- src/app/tmux.py
+++
@@ -1,9 +0,0 @@
-from .process import TmuxExecutor
-
-class Tmux(object):
-    def __init__(self, socket):
-        self.socket = socket
-        self.tmux = TmuxExecutor(socket)
-
-    def list_sessions(self):
-        return self.tmux('list-sessions')
