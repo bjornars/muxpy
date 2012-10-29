fc6c63ea2d70ed271dc9d44055a5c7dde89e58cb
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-10-28 20:21:59 -0700

    moved into a package

--- src/app/parser.py
+++ src/app/parser.py
@@ -7,3 +7,7 @@ class TmuxParser(object):
     def list_sessions(self):
         sessions = self.tmux.list_sessions()
         print sessions
+
+if __name__== '__main__':
+    tmux = TmuxParser('/tmp/tmux-1000/default')
+    print tmux.list_sessions()
