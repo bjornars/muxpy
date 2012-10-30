238c89e81b3d64fb495b8973721e6c7bc7c2b34b
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-10-30 12:02:56 -0700

    added writing of profiles

--- app/commands.py
+++ app/commands.py
@@ -1,5 +1,32 @@
+import os
+from collections import OrderedDict
+from pprint import pprint
+
+from app import profile, parser, formats
+
 def start(*args):
     print 'start', args
 
-def create(*args):
-    print 'create', args
+def create(p):
+    format = p.format
+    prof =  profile.get_profile_path(p.profile, p.format)
+    pname, fname = os.path.split(prof)
+    if not os.path.isdir(pname):
+        os.makedirs(pname)
+    
+    data = []
+
+    tmux = parser.TmuxParser(p.socket)
+    sessions = tmux.list_sessions()
+    
+    for session in sessions:
+        session_data = OrderedDict(name=session.name)
+        windows = tmux.list_windows(session.name)
+        session_data['windows'] = windows
+        data.append(session_data)
+    
+    formatted = formats.format(p.format, data)
+    with open(prof, 'w') as f:
+        f.write(formatted)
+
+    print 'made profile, %d sessions with total %d windows' % (len(data), sum(len(x['windows']) for x in data))
