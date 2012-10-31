3493f5b509704b37994ed7acf339e9b73510217c
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-10-31 08:43:04 -0700

    added format parsing, and options for more formats

--- app/formats.py
+++ app/formats.py
@@ -1,5 +1,13 @@
 import json
 
+from app import parser
+
+def parse(file_format, data):
+    if file_format == 'json':
+        return json.loads(data)
+    else:
+        raise NotImplementedError('there is no support yet for %s' % file_format)
+
 def format(file_format, data):
     if file_format == 'json':
         return json.dumps(data, indent=4)
