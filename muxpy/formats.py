2eb3da9b28cbf01c28948d2ee9ade73e57b17135
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-11-18 14:54:56 +0100

    refactored get_profile_path, added get_formats utility

--- muxpy/formats.py
+++ muxpy/formats.py
@@ -3,6 +3,16 @@ import json
 import logging
 logger = logging.getLogger()
 
+"""
+Ideas for this module: dynamically load all kinds of format -
+XML, YAML, S-lists, whateverthefuck really. On first use,
+make a list of available formats.
+
+Do not make any formats bring in hard dependencies, however."""
+
+def get_formats():
+    return ['json']
+
 def parse(file_format, data):
     if file_format == 'json':
         logger.info('reading %d bytes as json', len(data))
--- muxpy/profile.py
+++ muxpy/profile.py
@@ -1,12 +1,14 @@
 import os
 p = os.path
 
-
-def get_profile_path(profile, format):
+def get_profile_folder():
     user = p.expanduser('~')
-    path = p.join(user, '.muxpy', 'profiles', "%s.%s" % (profile, format))
+    path = p.join(user, '.muxpy', 'profiles')
     return p.abspath(path)
 
+def get_profile_path(profile, format):
+    return p.join(get_profile_folder(), "%s.%s" % (profile, format))
 
 if __name__ == '__main__':
+    print get_profile_folder()
     print get_profile_path('testprof', 'json')
